76d220c64b4828aae3e984d62a9c9c2e1561d1d6
Chron1um635 <chron1um522@gmail.com>
2024-10-19 18:13:56 +0300

    Initial Commit

---
+++ AnimatedIllustration/AnimatedIllustrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedIllustrationApp.swift
+//  AnimatedIllustration
+//
+//  Created by Максим Назаров on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedIllustrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
